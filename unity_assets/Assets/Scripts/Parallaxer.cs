2f3b13f990d16ce0e28fea4253fa25e58a1e8ba1
shirleyxzh <45251198+shirleyxzh@users.noreply.github.com>
2023-10-04 16:25:07 -0700

    add room option

--- unity_assets/Assets/Scripts/Parallaxer.cs
+++ unity_assets/Assets/Scripts/Parallaxer.cs
@@ -1,38 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using static System.Collections.Specialized.BitVector32;
+using static UnityEditor.PlayerSettings;
 using static UnityEngine.Rendering.CoreUtils;
 
 public class Parallaxer : MonoBehaviour
 {
     public float wallHeight = 1f;
+    public bool combineRooms = false;
 
     private Parallax[] sections;
     private Vector3 targetOffset = Vector3.zero;
+    private Bounds bounds;
 
-    private void Awake()
+    private void Start()
     {
         sections = GetComponentsInChildren<Parallax>();
+        foreach (var s in sections)
+        {
+            bounds.Encapsulate(s.bounds);
+        }
     }
 
     private void LateUpdate()
     {
+        var off = Vector3.zero;
         var cam = Camera.main.transform.position;
-        foreach (var s in sections)
+        if (combineRooms)
         {
-            if (s.InsideBounds(cam, out var off))
+            off = cam - bounds.center;
+            off = new Vector2(off.x / bounds.extents.x, off.y / bounds.extents.y);
+        }
+        else
+        {
+            foreach (var s in sections)
             {
-                off *= wallHeight;
-                targetOffset = new Vector3(off.x, 0, off.y);
-                break;
+                if (s.InsideBounds(cam, out off))
+                    break;
             }
         }
+        off *= wallHeight;
+        targetOffset = new Vector3(off.x, 0, off.y);
 
-        foreach (var section in sections)
+        foreach (var s in sections)
         {
-            section.OnUpdate(targetOffset);
+            s.OnUpdate(targetOffset);
         }
     }
 }
