1669fa4ee160a2e1142db2e62d484f59426d7b7b
shirleyxzh <45251198+shirleyxzh@users.noreply.github.com>
2023-10-11 23:21:41 -0700

    new lv1 scene + remove extra prefab

--- unity_assets/Assets/Scripts/AgentMover.cs
+++ unity_assets/Assets/Scripts/AgentMover.cs
@@ -19,7 +19,6 @@ public class AgentMover : MonoBehaviour
     private Vector3 xStep = Vector3.right * 0.16f;
     private Vector3 yStep = Vector3.up * 0.05f;
 
-    private NavMeshAgent navMesh;
     private bool fixupForNav;
 
     private void Awake()
@@ -28,7 +27,6 @@ public class AgentMover : MonoBehaviour
         defaultLayer = LayerMask.NameToLayer("Default");
         testMask = LayerMask.GetMask("enemy") | LayerMask.GetMask("wall");  // | LayerMask.GetMask("floor");
 
-        navMesh = GetComponent<NavMeshAgent>();
         fixupForNav = true;
     }
 
@@ -40,48 +38,29 @@ public class AgentMover : MonoBehaviour
         var step = MovementInput * maxSpeed * Time.deltaTime;
         var pos = transform.position;
         var newPos = pos + step;
-        //if (navMesh.isOnNavMesh)
-        //{
-        //    navMesh.SetDestination(newPos);
-        //}
-        //else
-        {
-            gameObject.layer = defaultLayer;
+        gameObject.layer = defaultLayer;
 
-            //if (step.x < 0 && Physics.Linecast(pos, newPos - xStep, testMask))
-            //    newPos.x = pos.x;
-            //else if (step.x > 0 && Physics.Linecast(pos, newPos + xStep, testMask))
-            //    newPos.x = pos.x;
-            //if (step.y < 0 && Physics.Linecast(pos, newPos - yStep, testMask))
-            //    newPos.y = pos.y;
-            //else if (step.y > 0 && Physics.Linecast(pos, newPos + yStep, testMask))
-            //    newPos.y = pos.y;
+        var dx = Mathf.Abs(step.x) + 0.05f;
+        var dir = step.x < 0 ? Vector3.left : Vector3.right;
+        var off1 = dir * dx;
+        var off2 = dir * (boxCol.bounds.size.x + dx);
+        var p1 = step.x < 0 ? boxCol.bounds.min : boxCol.bounds.max;
+        var p2 = step.x < 0 ? boxCol.bounds.max : boxCol.bounds.min;
+        if (testEdge(p1, p2, off1, off2))
+            newPos.x = pos.x;
 
-            var dx = Mathf.Abs(step.x) + 0.05f;
-            var dir = step.x < 0 ? Vector3.left : Vector3.right;
-            var off1 = dir * dx;
-            var off2 = dir * (boxCol.bounds.size.x + dx);
-            var p1 = step.x < 0 ? boxCol.bounds.min : boxCol.bounds.max;
-            var p2 = step.x < 0 ? boxCol.bounds.max : boxCol.bounds.min;
-            if (testEdge(p1, p2, off1, off2))
-                newPos.x = pos.x;
+        var dy = Mathf.Abs(step.y) + 0.05f;
+        dir = step.y < 0 ? Vector3.down : Vector3.up;
+        off1 = dir * dy;
+        off2 = dir * (boxCol.bounds.size.y + dy);
+        p1 = step.y < 0 ? boxCol.bounds.min : boxCol.bounds.max;
+        p2 = step.y < 0 ? boxCol.bounds.max : boxCol.bounds.min;
+        if (testEdge(p1, p2, off1, off2))
+            newPos.y = pos.y;
 
-            var dy = Mathf.Abs(step.y) + 0.05f;
-            dir = step.y < 0 ? Vector3.down : Vector3.up;
-            off1 = dir * dy;
-            off2 = dir * (boxCol.bounds.size.y + dy);
-            p1 = step.y < 0 ? boxCol.bounds.min : boxCol.bounds.max;
-            p2 = step.y < 0 ? boxCol.bounds.max : boxCol.bounds.min;
-            if (testEdge(p1, p2, off1, off2))
-                newPos.y = pos.y;
+        gameObject.layer = savedLayer;
 
-            gameObject.layer = savedLayer;
-
-            if (navMesh.isOnNavMesh)
-                navMesh.SetDestination(newPos);
-            else
-                transform.position = newPos;
-        }
+        transform.position = newPos;
     }
 
     private bool testEdge(Vector3 p1, Vector3 p2, Vector3 off1, Vector3 off2)
@@ -93,23 +72,7 @@ public class AgentMover : MonoBehaviour
     private void FixupForNav()
     {
         fixupForNav = false;
-        if (navMesh.isOnNavMesh)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                var child = transform.GetChild(i);
-                child.localPosition = new Vector3(child.localPosition.x, child.localPosition.z + 1f, child.localPosition.y);
-                child.localRotation = Quaternion.Euler(-90f, child.localRotation.eulerAngles.y, child.localRotation.eulerAngles.z);
-            }
-
-            boxCol = GetComponent<BoxCollider>();
-            boxCol.center = new Vector3(boxCol.center.x, boxCol.center.z + 1f, boxCol.center.y);
-            boxCol.size = new Vector3(boxCol.size.x, boxCol.size.z, boxCol.size.y);
-        }
-        else
-        {
-            transform.Translate(Vector3.back);
-            boxCol = GetComponent<BoxCollider>();
-        }
+        transform.Translate(Vector3.back);
+        boxCol = GetComponent<BoxCollider>();
     }
 }
